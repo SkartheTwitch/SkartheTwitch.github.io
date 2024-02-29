4672db203909f2293b52ff3a3353b1aa0ab201b5
steel <steel@DESKTOP-P54BUE6>
2024-02-29 00:22:05 -0500

    Add project files.

---
+++ scripts/init.lua
@@ -0,0 +1,14 @@
+Tracker:AddItems("items/items.json")
+
+ScriptHost:LoadScript("scripts/logic.lua")
+
+Tracker:AddMaps("maps/maps.json")
+
+Tracker:AddLocations("locations/logic.json")
+Tracker:AddLocations("locations/missions.json")
+
+Tracker:AddLayouts("layouts/options.json")
+Tracker:AddLayouts("layouts/item_grids.json")
+Tracker:AddLayouts("layouts/layout.json")
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
